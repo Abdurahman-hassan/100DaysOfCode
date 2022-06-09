eb4995e1215d3ffe600d7123b9cfdf3cbfb65f2e
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-06-09 13:09:49 +0200

    Revert "test"
    This reverts commit e67cd58f0be065366436267cd99a6236cdd3fbf7.

---
+++ Day3/3.3.leepYear/main.py
@@ -0,0 +1,11 @@
+year = int(input("Which year do you want to check? "))
+
+# return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+if (year % 4 == 0):
+    if (year % 100 != 0):
+        print("Leap year.")
+    elif (year % 400 == 0):
+        print("Leap year.")
+else:
+    print("Not Leap year.")
