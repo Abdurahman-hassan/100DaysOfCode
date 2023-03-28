88cd31d082132257eb4240e932aada63752d8b12
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-03-28 05:44:11 +0200

    Fix formatting

--- Day04/4.4.rock-paper-scissors/main.py
+++ Day04/4.4.rock-paper-scissors/main.py
@@ -37,14 +37,14 @@ print("Computer chose:")
 print(game_images[computer_choice])
 
 if user_choice >= 3 or user_choice < 0:
-  print("You typed an invalid number, you lose!")
+    print("You typed an invalid number, you lose!")
 elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
+    print("You win!")
 elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
+    print("You lose")
 elif computer_choice > user_choice:
-  print("You lose")
+    print("You lose")
 elif user_choice > computer_choice:
-  print("You win!")
+    print("You win!")
 elif computer_choice == user_choice:
-  print("It's a draw")
+    print("It's a draw")
